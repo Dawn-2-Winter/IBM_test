5be4bc67bb144733ef1087ed49055056f2a422ac
Dawn-2-Winter <91579901+Dawn-2-Winter@users.noreply.github.com>
2023-03-03 16:50:33 +0800

    Create solution.py

---
+++ Q1/solution.py
@@ -0,0 +1,57 @@
+
+
+len_shop = int(input())
+shop_str = input()
+shop_str = shop_str.strip().split(' ')
+
+len_request = int(input())
+request_str = input()
+request_str = request_str.strip().split(' ')
+
+def process(shops, requests):
+    count_avaliable = {}
+    for size in shops:
+        count_avaliable[size] = 0
+
+
+    for size in shops:
+        for i in range(1000):
+            t_size = 'X' * i + size
+            if t_size in count_avaliable:
+                count_avaliable[t_size] += 1
+                
+    for req in requests:
+        Yes = False
+        for i in range(1000):
+            if req[-1] == 'L':
+              t_size = 'X' * i + req
+              if t_size in count_avaliable :
+                  Yes = True
+                  break
+            elif req[-1] == 'S':
+              if i < len(req)-1:
+                t_size = req[i:]
+              else:break
+              if t_size in count_avaliable :
+                  Yes = True
+                  break
+            else:
+              if 'M' in count_avaliable :
+                  Yes = True
+                  break
+              else:
+                  t_size = 'X' * i + req
+                  if t_size in count_avaliable :
+                      Yes = True
+                      break
+        if not Yes:
+            return False
+
+    return True
+
+shops = ['XL', 'L', 'XXS', 'M', 'XXS']
+requests = ['XXXL', 'M', 'L', 'XXS']
+if process(shops, requests):
+    print("Yes")
+else:
+    print("No")
